57e5e5422f511677bee9abdf5b9277f404e51a64
Noe Perez <noeliaperez284@gmail.com>
2023-06-21 21:07:10 -0300

    cree controller,service y clase usuario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { UsuarioController } from './usuario/usuario.controller';
+import { UsuarioService } from './usuario/usuario.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client') }),],
+  controllers: [AppController, UsuarioController],
+  providers: [AppService, UsuarioService],
 })
 export class AppModule {}
