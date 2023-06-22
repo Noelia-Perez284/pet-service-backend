944e03077970be93f61a46bba6f4d155805aa1b9
Noe Perez <noeliaperez284@gmail.com>
2023-06-21 23:10:08 -0300

    cree el servicio de validar el usuario para el login

--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
+import { Usuario } from './usuario';
 
 @Injectable()
-export class UsuarioService {}
+export class UsuarioService {
+    private readonly usuarios:Usuario []=[
+        {
+            id: "1",
+            nombre: "Juan",
+            apellido: "Pérez",
+            email: "juan@example.com",
+            password: "contraseña123",
+            nombreMascota: "Max",
+            especieMascota: "Perro",
+            libretaSanitaria: "123456789"
+          },
+          {
+            id: "2",
+            nombre: "María",
+            apellido: "López",
+            email: "maria@example.com",
+            password: "abc123",
+            nombreMascota: "Luna",
+            especieMascota: "Gato",
+            libretaSanitaria: "987654321"
+          },
+          {
+            id: "3",
+            nombre: "Ana",
+            apellido: "García",
+            email: "ana@example.com",
+            password: "qwerty",
+            nombreMascota: "Pelusa",
+            especieMascota: "Gato",
+            libretaSanitaria: "456789123"
+          },
+          {
+            id: "4",
+            nombre: "Carlos",
+            apellido: "Ramírez",
+            email: "carlos@example.com",
+            password: "password123",
+            nombreMascota: "Rocky",
+            especieMascota: "Perro",
+            libretaSanitaria: "789123456"
+          },
+          {
+            id: "5",
+            nombre: "Luisa",
+            apellido: "Martínez",
+            email: "luisa@example.com",
+            password: "abcdef",
+            nombreMascota: "Nala",
+            especieMascota: "Gato",
+            libretaSanitaria: "321654987"
+        }
+    ];
+
+    validateUser(email: string, password: string): boolean {
+        const user = this.usuarios.find((u) => u.email === email);
+        
+        if (user && user.password === password) {
+            return true;
+        }
+        return false;
+    }
+    
+}
--- src/usuario/usuario.ts
+++ src/usuario/usuario.ts
@@ -7,4 +7,23 @@ export class Usuario {
     nombreMascota:string;
     especieMascota:string;
     libretaSanitaria:string;
+
+    constructor(id:string,
+        nombre:string,
+        apellido:string,
+        password:string,
+        nombreMascota:string,
+        especieMascota:string,
+        libretaSanitaria:string
+        
+        ){
+            this.id=id;
+            this.nombre=nombre;
+            this.apellido=apellido;
+            this.password=password;
+            this.nombreMascota=nombreMascota;
+            this.especieMascota=especieMascota;
+            this.libretaSanitaria=libretaSanitaria
+    }
+
 }
